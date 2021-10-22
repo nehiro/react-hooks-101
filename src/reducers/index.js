a35c98d1e6a7a6d3a7d91fb29073839a93cbc22c
nehiro <hiroaki.nerio@gmail.com>
2021-10-22 15:31:43 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,33 @@
+// action = {
+//   type: 'CRETATE_EVENT',
+//   title: '2020東京オリンピックのお知らせ',
+//   body: '2020年に東京でオリンピックを開催します！つきましては。。。'
+// }
+
+// # before
+// stete = []
+// # after
+// state= [
+//  {
+//   id: 1,
+//   title: '2020東京オリンピックのお知らせ',
+//   body: '2020年に東京でオリンピックを開催します！つきましては。。。'
+//  }
+// ]
+const events = (state = [], action)=>{
+  switch (action.type){
+    case 'CREATE_EVENT':
+      const event = {title: action.title, body: action.body};
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, {id, ...event}];
+    case 'DELETE_EVENT':
+      return state
+    case 'DELETE_ALL_EVENT':
+      return []
+    default:
+      return state
+  }
+}
+
+export default events;
